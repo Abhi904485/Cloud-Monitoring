02b236b9068fde9d90ff269b579cb5d459a76b95
Abhi904485 <Abhishek9044855265@gmail.com>
2018-08-29 22:43:44 +0530

    Adding dummy json and final web services helper methods and updated app.py

--- app.py
+++ app.py
@@ -4,54 +4,24 @@ from flask import Flask, jsonify, render_template, flash, redirect, url_for, req
 from flask_debugtoolbar import DebugToolbarExtension
 from forms import LoginForm, RegistrationForm
 from webservice_helper_method import ip_status, disk_status, all_process_status, network_usage, system_status, \
-    memory_status, start_service_remote, service_status, stop_service
+    memory_status, start_service_remote, service_status, stop_service, agent_list
+
+from threading import Thread
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
-computers = [
-    {
-        'ipaddress': '10.200.144.252',
-        'username': 'Administrator',
-        'passoword': 'test@123'
-    },
-    {
-        'ipaddress': '10.200.144.5',
-        'username': 'Administrator',
-        'password': 'novell@123'
-    },
-    {
-        'ipaddress': '10.200.144.5',
-        'username': 'Administrator',
-        'password': 'novell@123'
-    },
-    {
-        'ipaddress': '10.200.144.5',
-        'username': 'Administrator',
-        'password': 'novell@123'
-    },
-    {
-        'ipaddress': '10.200.144.5',
-        'username': 'Administrator',
-        'password': 'novell@123'
-    },
-    {
-        'ipaddress': '10.200.144.5',
-        'username': 'Administrator',
-        'password': 'novell@123'
-    },
-
-]
 computer_details = [
     {
-        'Networking Details':'',
+        'Networking Details': '',
         'username': 'Administrator',
         'passoword': 'test@123'
     },
 
 ]
+
 
 @app.route("/")
 def index():
@@ -60,7 +30,7 @@ def index():
 
 @app.route("/home")
 def home():
-    return render_template('home.html', computers=computers)
+    return render_template('home.html', computers=agent_list.get_agents())
 
 
 @app.route("/about")
@@ -91,49 +61,52 @@ def login():
 
 @app.route("/details")
 def details():
-    return render_template('details.html', title='details')
+    return render_template('details.html', title='details', ip_details=get_ip_details(),
+                           system_details=get_system_details(), disk_details=get_disk_details(),
+                           memory_details=get_memory_details(), process_details=get_all_process_details(),
+                           service_details=get_service_details(), network_details=get_network_details())
 
 
 @app.route('/get_ip_details', methods=['GET'])
 def get_ip_details():
-    response = ip_status.get_ip()
-    return jsonify(response)
+    response_ip = ip_status.get_ip()
+    return response_ip
 
 
 @app.route('/get_memory_details', methods=['GET'])
 def get_memory_details():
     response = memory_status.get_memory_usage()
-    return jsonify(response)
+    return response
 
 
 @app.route('/get_network_details', methods=['GET'])
 def get_network_details():
     response = network_usage.get_network()
-    return jsonify(response)
+    return response
 
 
 @app.route('/get_disk_details', methods=['GET'])
 def get_disk_details():
     response = disk_status.get_disk_usage()
-    return jsonify(response)
+    return response
 
 
 @app.route('/get_all_process_details', methods=['GET'])
 def get_all_process_details():
     response = all_process_status.process_list()
-    return jsonify(response)
+    return response
 
 
 @app.route('/get_system_details', methods=['GET'])
 def get_system_details():
     response = system_status.system_status()
-    return jsonify(response)
+    return response
 
 
 @app.route('/get_service_details', methods=['GET'])
 def get_service_details():
     response = service_status.service_list()
-    return jsonify(response)
+    return response
 
 
 @app.route('/start_service', methods=['POST'])
@@ -171,8 +144,14 @@ def stop_service():
 @app.route('/rdp', methods=['GET', 'POST'])
 def rdp():
     os.system(
-        "py -2 D:\\Python_hands_on\\Hackfest\\webservice_helper_method\\my_rdp.py -u Administrator -p novell@123 10.200.144.5:3389")
+        "py -2 D:\\Python_hands_on\\RAR\\webservice_helper_method\\my_rdp.py -u Administrator -p novell@123 10.200.144.5:3389")
     return redirect(url_for('details'))
+
+
+@app.route('/kill_process', methods=['GET', 'POST'])
+def kill_process():
+    searchword = request.args.get('name', '')
+    print(searchword)
 
 
 if __name__ == '__main__':
